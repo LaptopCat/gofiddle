1a716b18ebcb12b9d64fa9e906a37033f580b76b
Jonas <140553581+jnsdotgg@users.noreply.github.com>
2024-03-17 16:59:05 +0100

    [style] changed colors, layout, added window resizing, gave the terminal padding

--- assets/main.js
+++ assets/main.js
@@ -13,14 +13,18 @@ require.config({
 })
 
 require(["vs/editor/editor.main"], () => {
-    window.editor = monaco.editor.create(
-        document.getElementById("editor-container"),
-        {
-            fontFamily: "JetBrains Mono",
-            language: "go",
-            theme: "vs-dark",
-        }
-    )
+    window.editor = monaco.editor.create(document.getElementById("editor"), {
+        fontFamily: "JetBrains Mono",
+        language: "go",
+        theme: "vs-dark",
+        automaticLayout: true,
+        cursorBlinking: "smooth",
+        smoothScrolling: true,
+        fontSize: 13,
+        minimap: {
+            enabled: false,
+        },
+    })
 
     editor.setValue(`package main
     
@@ -33,27 +37,27 @@ func main() {
 
 window.term = new Terminal({
     theme: {
-        foreground: '#eff0eb',
-        background: '#282a36',
-        selection: '#97979b33',
-        black: '#282a36',
-        brightBlack: '#686868',
-        red: '#ff5c57',
-        brightRed: '#ff5c57',
-        green: '#5af78e',
-        brightGreen: '#5af78e',
-        yellow: '#f3f99d',
-        brightYellow: '#f3f99d',
-        blue: '#57c7ff',
-        brightBlue: '#57c7ff',
-        magenta: '#ff6ac1',
-        brightMagenta: '#ff6ac1',
-        cyan: '#9aedfe',
-        brightCyan: '#9aedfe',
-        white: '#f1f1f0',
-        brightWhite: '#eff0eb'
+        foreground: "#eff0eb",
+        background: "#282a36",
+        selection: "#97979b33",
+        black: "#282a36",
+        brightBlack: "#686868",
+        red: "#ff5c57",
+        brightRed: "#ff5c57",
+        green: "#5af78e",
+        brightGreen: "#5af78e",
+        yellow: "#f3f99d",
+        brightYellow: "#f3f99d",
+        blue: "#57c7ff",
+        brightBlue: "#57c7ff",
+        magenta: "#ff6ac1",
+        brightMagenta: "#ff6ac1",
+        cyan: "#9aedfe",
+        brightCyan: "#9aedfe",
+        white: "#f1f1f0",
+        brightWhite: "#eff0eb",
     },
-    fontFamily: "JetBrains Mono"
+    fontFamily: "JetBrains Mono",
 })
 Defer(() => {
     term.open(document.getElementById("terminal"))
@@ -67,7 +71,7 @@ Defer(() => {
 function exec() {
     term.clear()
     term.writeln("\x1b[1mExecuting...\x1b[0m\r\n")
-    
+
     start = performance.now()
     res = ExecPure(editor.getValue())
     end = performance.now()
---
+++ assets/window-resizing.js
@@ -0,0 +1,34 @@
+const handle = document.getElementById("handle")
+const container = document.getElementById("container")
+const outerEditor = document.getElementById("outer-editor")
+const outerPreview = document.getElementById("outer-terminal")
+
+handle.addEventListener("dblclick", () => {
+    container.style.gridTemplateColumns = "1fr 1rem 1fr"
+})
+let isDragging = false
+handle.addEventListener("mousedown", () => {
+    isDragging = true
+    outerPreview.style.pointerEvents = "none"
+})
+document.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+        document.body.style.cursor = "ew-resize"
+        let rightSide = window.innerWidth - e.clientX
+        let leftSide = e.clientX
+        container.style.gridTemplateColumns = `${leftSide - 8}px 1rem ${
+            rightSide - 8
+        }px`
+    } else {
+        document.body.style.cursor = ""
+    }
+})
+document.addEventListener("mouseup", () => {
+    if (isDragging) {
+        isDragging = false
+        outerPreview.style.pointerEvents = ""
+    }
+})
+outerPreview.addEventListener("mouseup", () => {
+    isDragging = false
+})
